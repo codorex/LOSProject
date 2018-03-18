1a0ee06d522e91768e8b50de88d04fcd97623775
AlexPlom <alexiosplomaritis@gmail.com>
2018-03-18 18:48:14 +0200

    Created customEmailClient

---
+++ LOS/LOS.EmailClient/CustomEmailClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace LOS.EmailClient
+{
+    class CustomEmailClient
+    {
+        private readonly MailAddress fromAddress = new MailAddress("longboardonlinestore@gmail.com");
+        private readonly string fromPassword = "123admin";
+
+        public void SendEmail(MailAddress addressToSendTo, string subject, string body)
+        {
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+
+            };
+
+            using (var message = new MailMessage(fromAddress, addressToSendTo)
+            {
+                Subject = subject,
+                Body = body
+            })
+
+                smtp.Send(message);
+        }
+    }
+}
